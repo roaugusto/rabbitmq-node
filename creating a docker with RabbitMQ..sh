401f16660e13b206e61b57a4680f5adbbcbf76d4
Rodrigo Santos <ro.augusto@gmail.com>
2019-10-14 23:08:52 -0300

    first commit

--- creating a docker with RabbitMQ..sh
+++ creating a docker with RabbitMQ..sh
@@ -1,7 +1,3 @@
-# rabbitmq-node
-Example nodejs and rabbitmq
-
 # Comando para criar e executar uma instância do rabbitmq
 docker run -d --hostname my-rabbit --name rabbit13 -p 8080:15672 -p 5672:5672 -p 25676:25676 rabbitmq:3-management
-
 
